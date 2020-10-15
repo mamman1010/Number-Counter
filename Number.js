166bf66fbe09cca8a78dae3fffdf4baa75b6830c
mamman1010 <mammanharuna@gmail.com>
2020-10-15 17:05:39 +0100

    Very basic counter

---
+++ Number.js
@@ -0,0 +1,9 @@
+variable = 0;
+
+function increase(){
+document.getElementById("numbercount").innerHTML= variable += 1;
+}
+
+function decrease(){
+    document.getElementById("numbercount").innerHTML = variable -= 1;
+}
